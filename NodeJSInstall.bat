f17cbb7836c0911ebd82f6fa86db528b47ed88c9
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-13 10:22:45 +0700

    Update

---
+++ NodeJSInstall.bat
@@ -0,0 +1,14 @@
+
+
+
+set NodeJSVersion=v18.16.1
+
+
+
+echo %PATH%
+cd %USERPROFILE%\Downloads
+curl -kLO --retry 333 https://nodejs.org/dist/%NodeJSVersion%/node-%NodeJSVersion%-x64.msi
+msiexec.exe /i node-%NodeJSVersion%-x64.msi ADDLOCAL=ALL /qn /passive /promptrestart /L*vx %USERPROFILE%\Downloads\node-%NodeJSVersion%-x64.txt
+echo %PATH%
+
+
