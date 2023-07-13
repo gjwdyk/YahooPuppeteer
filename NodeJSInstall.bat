5baeaf2f2f36cf0c3437a65e0399493a0c1eb243
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-13 15:54:03 +0700

    Update

--- NodeJSInstall.bat
+++ NodeJSInstall.bat
@@ -1,22 +1,27 @@
 
 
 
+:: Set the Node.JS version to be installed
 set NodeJSVersion=v18.16.1
 
 
 
 echo %PATH%
+:: Download the Node.JS installer .msi file to %USERPROFILE%\Downloads folder
 cd %USERPROFILE%\Downloads
 curl -kLO --retry 333 https://nodejs.org/dist/%NodeJSVersion%/node-%NodeJSVersion%-x64.msi
+:: Install the Node.JS installer .msi file
 msiexec.exe /i node-%NodeJSVersion%-x64.msi ADDLOCAL=ALL /qn /passive /promptrestart /L*vx %USERPROFILE%\Downloads\node-%NodeJSVersion%-x64.txt
 echo %PATH%
 
 
 
+:: The followings force the current shell to refresh the environment variables, especially %PATH% variable which is needed to run Node.JS
 curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/ResetVariables.vbs
 curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/ResetVariables.bat
 call ResetVariables.bat
 echo %PATH%
+:: Test a Node.JS command
 node -v
 
 
