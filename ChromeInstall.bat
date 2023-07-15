ebb3d50b04877888d0c5f432bfd793fb16a9e34b
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-15 23:55:45 +0700

    Update

--- ChromeInstall.bat
+++ ChromeInstall.bat
@@ -1,27 +1,30 @@
 
 
 
+rem +-+--------------------------------------+-+
+rem +-+--------------------------------------+-+
+rem ! ! Install Common/Normal Chrome Browser ! !
+rem +-+--------------------------------------+-+
+rem +-+--------------------------------------+-+
+
 rem The built-in Chrome browser which Puppeteer brings-in with :
-rem have a more difficult installation path to deal with in terms of scripting (i.e. have version number in the path)
-rem When run, the browser actually named "Google Chrome for Testing"
-
-rem Therefore installing normal/common Chrome Browser
-
-
-
-rem Location of Chrome Browser which is brought by Puppeteer (version may change) :
+rem (1) Have a more difficult installation path to deal with in terms of scripting (i.e. have version number in the path).
+rem (2) When run, the browser actually named "Google Chrome for Testing".
+rem
+rem Therefore installing normal/common Chrome browser.
+rem
+rem Location of Chrome browser which is brought by Puppeteer (version may change) :
 rem C:\Users\HC\.cache\puppeteer\chrome\win64-114.0.5735.133\chrome-win64\chrome.exe
-rem Need to escape '\' into '\\'
+rem When used in Puppeteer script(s), you may need to escape '\' into '\\'
 rem C:\\Users\\HC\\.cache\\puppeteer\\chrome\\win64-114.0.5735.133\\chrome-win64\\chrome.exe
-rem You can also use normal Chrome Browser which you install manually, which usually will be located at :
+rem
+rem You can use common/normal Chrome browser which you (commonly/normally) install manually, which usually will be located at :
 rem C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe
 
 
 
-
-
-
 set CommonNormalChromeDefaultPathFile="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set InstallModule=Google.Chrome
 
 
 
@@ -29,16 +32,68 @@ cd %USERPROFILE%\Downloads
 
 if exist %CommonNormalChromeDefaultPathFile% (
  echo "Chrome Browser had been installed at %CommonNormalChromeDefaultPathFile% ."
+ PreviousChromeInstallationExist=Yes
 ) else (
- echo "Can NOT Find %CommonNormalChromeDefaultPathFile% . Try to download and install common normal Chrome browser."
- powershell.exe -command "winget install google.chrome"
+ echo "Can NOT find %CommonNormalChromeDefaultPathFile% ."
+ PreviousChromeInstallationExist=No
+)
 
- if exist %CommonNormalChromeDefaultPathFile% (
-  echo "Chrome Browser had been installed at %CommonNormalChromeDefaultPathFile% ."
-  powershell.exe -command "New-NetFirewallRule -DisplayName CommonNormalChromeBrowser -Direction Inbound -Program %CommonNormalChromeDefaultPathFile% -Action Allow"
+if [%PuppeteerInstall%]==[Successful] (
+ if [%PreviousChromeInstallationExist%]==[Yes] (
+  set DoChromeInstallation=No
  ) else (
-  echo "Apparently installation of Chrome Browser had failed."
+  set DoChromeInstallation=Yes
+ )
+) else (
+ if [%PuppeteerInstall%]==[Success] (
+  if [%PreviousChromeInstallationExist%]==[Yes] (
+   set DoChromeInstallation=No
+  ) else (
+   set DoChromeInstallation=Yes
+  )
+ ) else (
+  set DoChromeInstallation=No
  )
 )
+
+
+
+if [%DoChromeInstallation%]==[Yes] (
+ winget install --id %InstallModule% --accept-package-agreements --accept-source-agreements --verbose-logs --log %USERPROFILE%\Downloads\%InstallModule%.txt
+ if %ERRORLEVEL% EQU 0 (
+  set ChromeInstall=Success
+ ) else (
+  set ChromeInstall=Fail
+ )
+) else (
+)
+if [%DoChromeInstallation%]==[Yes] ( echo "%InstallModule% installation %ChromeInstall% ." )
+
+
+
+
+
+
+rem powershell.exe -command "New-NetFirewallRule -DisplayName CommonNormalChromeBrowser -Direction Inbound -Program %CommonNormalChromeDefaultPathFile% -Action Allow"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rem +-+--------------------------------------------------+-+
+rem +-+--------------------------------------------------+-+
+rem ! ! End of Common/Normal Chrome Browser Installation ! !
+rem +-+--------------------------------------------------+-+
+rem +-+--------------------------------------------------+-+
 
 
