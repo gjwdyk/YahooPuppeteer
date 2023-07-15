ce98ab6672806e59addff1ddc4d3042ca49ea050
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-15 23:08:46 +0700

    Update

--- ChromeInstall.bat
+++ ChromeInstall.bat
@@ -9,22 +9,22 @@
 
 
 
-set NormalChromeDefaultPathFile="C:\Program Files\Google\Chrome\Application\chrome.exe"
+set CommonNormalChromeDefaultPathFile="C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 
 
 cd %USERPROFILE%\Downloads
 
-IF EXIST %NormalChromeDefaultPathFile% (
- echo "Chrome Browser had been installed at %NormalChromeDefaultPathFile% ."
-) ELSE (
- echo "Can NOT Find %NormalChromeDefaultPathFile% . Try to download and install common normal Chrome browser."
+if exist %CommonNormalChromeDefaultPathFile% (
+ echo "Chrome Browser had been installed at %CommonNormalChromeDefaultPathFile% ."
+) else (
+ echo "Can NOT Find %CommonNormalChromeDefaultPathFile% . Try to download and install common normal Chrome browser."
  powershell.exe -command "winget install google.chrome"
 
- IF EXIST %NormalChromeDefaultPathFile% (
-  echo "Chrome Browser had been installed at %NormalChromeDefaultPathFile% ."
-  powershell.exe -command "New-NetFirewallRule -DisplayName CommonNormalChromeBrowser -Direction Inbound -Program %NormalChromeDefaultPathFile% -Action Allow"
- ) ELSE (
+ if exist %CommonNormalChromeDefaultPathFile% (
+  echo "Chrome Browser had been installed at %CommonNormalChromeDefaultPathFile% ."
+  powershell.exe -command "New-NetFirewallRule -DisplayName CommonNormalChromeBrowser -Direction Inbound -Program %CommonNormalChromeDefaultPathFile% -Action Allow"
+ ) else (
   echo "Apparently installation of Chrome Browser had failed."
  )
 )
