26589f89fbc0c6c21d9f5e47ea433bd993968430
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-15 12:43:39 +0700

    Update

--- NodeJSInstall.bat
+++ NodeJSInstall.bat
@@ -88,11 +88,11 @@ rem Official Token for Comment is Rem .
 rem Using :: for comment may emit syntax error, example within a block statement.
 rem
 rem Within a block statement (a parenthesised series of statements), the entire block is parsed and then executed.
-rem Any %var% within the block will be replaced by that variable's value at the time the block is parsed - before the block is executed - the same thing applies to a FOR ... DO (block).
-rem Hence, IF (something) else (somethingelse) will be executed using the values of %variables% at the time the IF is encountered.
+rem Any %variable% within the block will be replaced by that variable's value at the time the block is parsed - before the block is executed - the same thing applies to a FOR ... DO (block).
+rem Hence, IF (do-this) else (do-that) will be executed using the values of %variable% at the time the IF is encountered.
 rem
 rem Two common ways to overcome this are :
-rem (1) to use setlocal enabledelayedexpansion and use !var! in place of %var% to access the changed value of var
+rem (1) to use setlocal enabledelayedexpansion and use !variable! in place of %variable% to access the changed value of variable,
 rem or
 rem (2) to call a subroutine to perform further processing using the changed values.
 
