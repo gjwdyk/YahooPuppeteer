47856406b5bb64ed52ec4633778630ee53526a31
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-15 11:43:06 +0700

    Update

--- NodeJSInstall.bat
+++ NodeJSInstall.bat
@@ -1,15 +1,15 @@
 
 
 
-:: ╔═╦═════════════════╦═╗
-:: ╠═╬═════════════════╬═╣
-:: ║ ║ Install Node.JS ║ ║
-:: ╠═╬═════════════════╬═╣
-:: ╚═╩═════════════════╩═╝
+rem ╔═╦═════════════════╦═╗
+rem ╠═╬═════════════════╬═╣
+rem ║ ║ Install Node.JS ║ ║
+rem ╠═╬═════════════════╬═╣
+rem ╚═╩═════════════════╩═╝
 
-:: ╔══════════════╗
-:: ║ Using WinGet ║
-:: ╚══════════════╝
+rem ╔══════════════╗
+rem ║ Using WinGet ║
+rem ╚══════════════╝
 
 cd %USERPROFILE%\Downloads
 set InstallModule=OpenJS.NodeJS.LTS
@@ -21,51 +21,51 @@ if %ERRORLEVEL% EQU 0 (
 )
 echo "%InstallModule% installation %NodeJSInstall% ."
 
-:: ╔════════════════════════╗
-:: ║ Using cURL and MSIExec ║
-:: ╚════════════════════════╝
+rem ╔════════════════════════╗
+rem ║ Using cURL and MSIExec ║
+rem ╚════════════════════════╝
 
-:: :: Set the Node.JS version to be installed
-:: set NodeJSVersion=v18.16.1
+rem rem Set the Node.JS version to be installed
+rem set NodeJSVersion=v18.16.1
 ::
-:: :: Download the Node.JS installer .msi file to %USERPROFILE%\Downloads folder
-:: cd %USERPROFILE%\Downloads
-:: curl -kLO --retry 333 https://nodejs.org/dist/%NodeJSVersion%/node-%NodeJSVersion%-x64.msi
-:: :: Install the Node.JS installer .msi file
-:: msiexec.exe /i node-%NodeJSVersion%-x64.msi ADDLOCAL=ALL /qn /passive /promptrestart /L*vx %USERPROFILE%\Downloads\node-%NodeJSVersion%-x64.txt
-:: if %ERRORLEVEL% EQU 0 (
-::  set NodeJSInstall=Success
-:: ) else (
-::  set NodeJSInstall=Fail
-:: )
-:: Echo "%InstallModule% installation %NodeJSInstall% ."
+rem rem Download the Node.JS installer .msi file to %USERPROFILE%\Downloads folder
+rem cd %USERPROFILE%\Downloads
+rem curl -kLO --retry 333 https://nodejs.org/dist/%NodeJSVersion%/node-%NodeJSVersion%-x64.msi
+rem rem Install the Node.JS installer .msi file
+rem msiexec.exe /i node-%NodeJSVersion%-x64.msi ADDLOCAL=ALL /qn /passive /promptrestart /L*vx %USERPROFILE%\Downloads\node-%NodeJSVersion%-x64.txt
+rem if %ERRORLEVEL% EQU 0 (
+rem  set NodeJSInstall=Success
+rem ) else (
+rem  set NodeJSInstall=Fail
+rem )
+rem Echo "%InstallModule% installation %NodeJSInstall% ."
 
 
 
-:: ╔═╦═══════════════════════════╦═╗
-:: ╠═╬═══════════════════════════╬═╣
-:: ║ ║ Test Node.JS Installation ║ ║
-:: ╠═╬═══════════════════════════╬═╣
-:: ╚═╩═══════════════════════════╩═╝
+rem ╔═╦═══════════════════════════╦═╗
+rem ╠═╬═══════════════════════════╬═╣
+rem ║ ║ Test Node.JS Installation ║ ║
+rem ╠═╬═══════════════════════════╬═╣
+rem ╚═╩═══════════════════════════╩═╝
 
 if [%NodeJSInstall%] == [Success] (
 
-:: ╔═══════════════════════════════╗
-:: ║ Fix Environment Variable PATH ║
-:: ╚═══════════════════════════════╝
+ rem ╔═══════════════════════════════╗
+ rem ║ Fix Environment Variable PATH ║
+ rem ╚═══════════════════════════════╝
 
-:: The followings force the current shell to refresh the environment variables, especially %PATH% variable which is needed to run Node.JS
+ rem The followings force the current shell to refresh the environment variables, especially PATH variable which is needed to run Node.JS
  curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/ResetVariables.vbs
  curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/ResetVariables.bat
  call ResetVariables.bat
  echo %PATH%
 
-:: ╔═══════════════════════════╗
-:: ║ Test: Get Node.JS Version ║
-:: ╚═══════════════════════════╝
+ rem ╔═══════════════════════════╗
+ rem ║ Test: Get Node.JS Version ║
+ rem ╚═══════════════════════════╝
 
  for /F %%f in ('node -v') do set NodeJSVersion=%%f
- if [%NodeJSVersion%]==[] (
+ if [%NodeJSVersion%] == [] (
   echo "Installed Node.JS version is : [%NodeJSVersion%] ."
  ) else (
   echo "Installed Node.JS version is : %NodeJSVersion% ."
@@ -77,10 +77,10 @@ if [%NodeJSInstall%] == [Success] (
 
 
 
-:: ╔═╦═════════════════╦═╗
-:: ╠═╬═════════════════╬═╣
-:: ║ ║ End of Document ║ ║
-:: ╠═╬═════════════════╬═╣
-:: ╚═╩═════════════════╩═╝
+rem ╔═╦═════════════════╦═╗
+rem ╠═╬═════════════════╬═╣
+rem ║ ║ End of Document ║ ║
+rem ╠═╬═════════════════╬═╣
+rem ╚═╩═════════════════╩═╝
 
 
