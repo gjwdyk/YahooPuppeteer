959ceca7d98b9aafa799e769e5e998f52103cfed
何も無い <64021730+gjwdyk@users.noreply.github.com>
2023-07-16 13:17:43 +0700

    Update

--- Wrapper.bat
+++ Wrapper.bat
@@ -9,5 +9,6 @@ cd %USERPROFILE%\Downloads
 curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/NodeJSInstall.bat && call NodeJSInstall.bat
 curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/PuppeteerInstall.bat && call PuppeteerInstall.bat
 curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/ChromeInstall.bat && call ChromeInstall.bat
+curl -kLO --retry 333 https://raw.githubusercontent.com/gjwdyk/YahooPuppeteer/main/arcadia-example.js && call node arcadia-example.js
 
 
